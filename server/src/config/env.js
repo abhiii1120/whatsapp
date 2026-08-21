4597d64a456135fac9ad968d8c44fc69bb6655d0
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-08-21 17:37:49 +0530

    added database connnectivity and env js to validate our envs and added some constants and pino logger

---
+++ server/src/config/env.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+import z from "zod";
+import logger from "./logger.js";
+import appConstant from "../constant/app.constant.js";
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(appConstant.PORT),
+  MONGO_URI:z.string()
+});
+
+const {success,data,error} = envSchema.safeParse(process.env);
+
+if(!success){
+    logger.info("Invalid env varaibles",error.format());
+    process.exit(1);
+}
+
+export default Object.freeze(data);
