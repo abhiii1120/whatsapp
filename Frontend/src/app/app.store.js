9e49a31183088ae9b0789135cce9c3b801ed5a95
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-08-25 18:49:39 +0530

    added axios , and setup of basic redux store and auth slices

---
+++ Frontend/src/app/app.store.js
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/state/auth.slice";
+export let store = configureStore({
+  reducer: authReducer,
+});
---
+++ Frontend/src/features/auth/state/auth.slice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name:'auth',
+    initialState:{
+        user:null,
+        accessToken:null,
+        loading:false,
+        error:null,
+    },
+    reducers:{
+        setUser : (state,action) => {
+            state.user = action.payload;
+        },
+        setAccessToken : (state,action) => {
+            state.accessToken = action.payload;
+        },
+        setLoading:(state,action) => {
+            state.loading = action.payload;
+        },
+        setError:(state,action) => {
+            state.error =action.payload
+        }
+    }
+})
+
+export const {setUser,setAccessToken,setError,setLoading} = authSlice.actions;
+export default authSlice.reducer;
