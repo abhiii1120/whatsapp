509c09eee7e40497d8876395a0cce724b68f57e3
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-08-28 23:09:35 +0530

    added socket setup so two users can communicate between themselves

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,22 @@ import connectDB from "./src/config/db.js";
 import logger from "./src/config/logger.js";
 import env from "./src/config/env.js";
 import { createRedisClient } from "./src/config/cache.js";
-// --- database ---
+import {createServer} from 'http';
+import {initializeSocketServer} from "./src/sockets/socket.server.js";
+ 
+const server = createServer(app);
+
+// --- connect to database ---
 await connectDB();
 
+// --- connect to redis ---
 createRedisClient();
 
 let PORT = env.PORT
 
-app.listen(PORT, () => {
+// --- initialize socket server ---
+initializeSocketServer(server);
+
+server.listen(PORT, () => {
   logger.info({ PORT: PORT }, "server is running on port 3000");
 });
